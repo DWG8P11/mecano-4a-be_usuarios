f222964d6ec4ef4e5a2ffeb0fd2f7140a6ecc745
Andrea Quebrada <danseurwaslala@gmail.com>
2021-11-17 11:42:58 -0500

    arreglo de error en init de las vistasta y en importaciones de urls

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -3,12 +3,12 @@
 from django.contrib                 import admin
 from django.urls                    import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView )
-from back_App                       import views
+from back_App.views.userView        import (UserCreateView, UserDetailView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('user/', views.userView.UserCreateView.as_view()),
-    path('user/<int:id_user_url>/',views.userView.UserDetailView.as_view()),
+    path ('user/', UserCreateView.as_view()),
+    path('user/<int:id_user_url>/',UserDetailView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
 ]
